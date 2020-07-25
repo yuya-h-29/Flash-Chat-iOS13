8c37e8472b0f0dfc0cb59ddbee4611af65e34cb6
yuya-h-29 <tzou11291996@gmail.com>
2020-07-25 23:10:52 +0900

    adding firebase login

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,13 +14,23 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Yo!"),
+        Message(sender: "a@b.com", body: "Hey!"),
+        Message(sender: "1@2.com", body: "Whaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaat's up!")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        tableView.delegate = self
+        tableView.dataSource = self
         
         title = K.appName
         
         navigationItem.hidesBackButton = true
 
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -39,3 +49,23 @@ class ChatViewController: UIViewController {
     }
     
 }
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label.text = messages[indexPath.row].body
+        
+        return cell
+    }
+
+}
+
+//extension ChatViewController: UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//}
